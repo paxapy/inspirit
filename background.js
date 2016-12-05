3011860aa9dabbe634d22ec01a81a3e07c54e48b
paxapy <paxapy@gmail.com>
2016-12-05 21:24:18 +0300

    Initial commit

---
+++ background.js
@@ -0,0 +1,24 @@
+function createSpirit () {
+    chrome.storage.sync.set({'wedone': false});
+    chrome.tabs.create({'url': chrome.extension.getURL('spirit.html'), 'selected': true});
+}
+
+function focusOnSpirit() {
+    chrome.tabs.query({'url': chrome.extension.getURL('spirit.html')}, function (tabs) {
+        !tabs.length && createSpirit();
+        tabs.length && chrome.tabs.update(tabs[0].id, {'selected': true});
+    });
+}
+
+chrome.tabs.onActivated.addListener(function() {
+    chrome.storage.sync.get(function (data) {
+        var count = data.tabsCount || 0;
+        var nextTime = data.nextTime || count;
+        console.log(count, nextTime);
+        chrome.storage.sync.set({'tabsCount': count += 1});
+        if (count > nextTime) {
+            data.wedone = false;
+        }
+        !data.wedone && focusOnSpirit()
+    });
+});
---
+++ spirit.js
@@ -0,0 +1,97 @@
+GREETINGS = [
+    'Нажми ретурн и проходи',
+    'Слишком много кликов',
+    'Пришло время поговорить с интернетом',
+    'Может быть help сможет тебе помочь',
+    'Что ты сделал для российского сегмента в свои годы',
+    'Не накликать бы беду',
+    'Люди так много говорят. Что они спрашивают?',
+    'The Matrix has you...'
+];
+var queries = [];
+var tries = 0;
+var tabsCount;
+var allInput;
+var currentQuery;
+var currentInput;
+
+function weDone() {
+    $('#edward').slideDown(function () {
+        chrome.storage.sync.set({'wedone': true});
+        chrome.storage.sync.set({'allInput': allInput + currentInput});
+        chrome.storage.sync.set({'nextTime': tabsCount + currentInput.length});
+        chrome.tabs.update({url: 'https://yandex.ru/yandsearch?text=' + currentQuery, 'selected': true})
+    });
+}
+
+function weGood(input) {
+    chrome.storage.sync.set({'nextTime': tabsCount + input.length + Math.floor(Math.random()*42)});
+    return input.length > 142
+}
+
+function getQueries() {
+    queries = [];
+    $.get('http://export.yandex.ru/last/last20x.xml', function(res) {
+        $(res).find("item").each(function(){
+            queries.push($(this).text());
+        })
+    })
+}
+
+chrome.storage.sync.get(function (data) {
+    tabsCount = data.tabsCount || 0;
+    allInput = data.allInput || '';
+});
+
+$(function () {
+    getQueries();
+    $('#terminal').terminal(function(input, term) {
+        currentInput += ' ' + input;
+        if (tries === 0 && tabsCount) {
+            term.echo('Я показал тебе чуть менее чем ' + tabsCount + ' интернетов')
+        }
+        if (tries % 7 === 0) {
+            term.echo('Иногда мне везет')
+        } else if (input === "I'm feeling lucky") {
+            weDone(currentInput)
+        } else if (input === 'help') {
+            term.echo('Расскажи мне что-нибудь и что-нибудь расскажу тебе я')
+        } else if (weGood(currentInput + input)) {
+            weDone(currentInput)
+        } else {
+            term.echo(currentQuery + '?')
+        }
+        tries++;
+    }, {
+        greetings: GREETINGS[Math.floor(Math.random()*GREETINGS.length)],
+        name: 'whatever',
+        height: '50vh',
+        prompt: 'ntsprt> '
+    });
+});
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+
+c.height = 420;
+c.width = window.innerWidth;
+
+var font_size = 10;
+
+function draw()
+{
+	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+	ctx.fillRect(0, 0, c.width, c.height);
+
+	ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	ctx.font = font_size + "px";
+
+    var query = queries[Math.floor(Math.random()*queries.length)];
+    currentQuery = query;
+    ctx.fillText(query, Math.floor(Math.random()*c.width), Math.floor(Math.random()*c.height));
+}
+
+setInterval(draw, 88);
+
+
+
