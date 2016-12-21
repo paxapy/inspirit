2adf7118e299844c7946b11c83e497b5a2df91ef
paxapy <paxapy@gmail.com>
2016-12-22 02:31:48 +0300

    another bunch of changes, improvements

--- spirit.js
+++ spirit.js
@@ -1,37 +1,31 @@
-GREETINGS = [
-    'Нажми ретурн и проходи',
-    'Слишком много кликов',
-    'Пришло время поговорить с интернетом',
-    'Может быть help сможет тебе помочь',
-    'Что ты сделал для российского сегмента в свои годы',
-    'Не накликать бы беду',
-    'Люди так много говорят. Что они спрашивают?',
-    'The Matrix has you...'
-];
+var greetings = 'Добро пожаловать...';
 var queries = [];
 var tries = 0;
 var tabsCount;
 var allInput;
 var currentQuery;
-var currentInput;
+var currentInput = '';
+
+var Twit = require('twit');
+
+
 
 function weDone() {
     $('#edward').slideDown(function () {
         chrome.storage.sync.set({'wedone': true});
         chrome.storage.sync.set({'allInput': allInput + currentInput});
-        chrome.storage.sync.set({'nextTime': tabsCount + currentInput.length});
+        chrome.storage.sync.set({'nextTime': tabsCount + currentInput.length + Math.floor(Math.random()*42)});
         chrome.tabs.update({url: 'https://yandex.ru/yandsearch?text=' + currentQuery, 'selected': true})
     });
 }
 
 function weGood(input) {
-    chrome.storage.sync.set({'nextTime': tabsCount + input.length + Math.floor(Math.random()*42)});
-    return input.length > 142
+    return input.length > 10 + Math.floor(Math.random()*142)
 }
 
 function getQueries() {
-    queries = [];
     $.get('http://export.yandex.ru/last/last20x.xml', function(res) {
+        queries = [];
         $(res).find("item").each(function(){
             queries.push($(this).text());
         })
@@ -41,32 +35,32 @@ function getQueries() {
 chrome.storage.sync.get(function (data) {
     tabsCount = data.tabsCount || 0;
     allInput = data.allInput || '';
+    if (tabsCount) {greetings += ' снова.'}
 });
 
 $(function () {
     getQueries();
     $('#terminal').terminal(function(input, term) {
-        currentInput += input + ' ';
         if (input === "I'm feeling lucky") {
             weDone(currentInput)
-        }
-        if (tries === 0 && tabsCount) {
-            term.echo('Я показал тебе чуть менее чем ' + tabsCount + ' интернетов')
-        } else if (tries % 7 === 0) {
+        } else if (input === '') {
+            term.echo('У меня много вопросов')
+        } else if (tries && tries % 7 === 0) {
             term.echo('Иногда мне везет')
         } else if (input === 'help') {
-            term.echo('Расскажи мне что-нибудь и что-нибудь расскажу тебе я')
+            term.echo('Тебе нужно что-то придумать, что бы выбраться отсюда')
         } else if (weGood(currentInput + input)) {
             weDone(currentInput)
         } else {
-            term.echo(currentQuery + '?')
+            term.echo(currentQuery + '?');
         }
         tries++;
+        currentInput += ' ' + input;
     }, {
-        greetings: GREETINGS[Math.floor(Math.random()*GREETINGS.length)],
+        greetings: greetings,
         name: 'whatever',
         height: '50vh',
-        prompt: 'ntsprt> '
+        prompt: 'net@interface$ '
     });
 });
 
@@ -76,22 +70,24 @@ var ctx = c.getContext("2d");
 c.height = 420;
 c.width = window.innerWidth;
 
-var font_size = 10;
-
-function draw()
-{
+function draw() {
+    if (!queries.length) {return}
+    var font_size = Math.floor(Math.random()*13 + 8);
 	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
 	ctx.fillRect(0, 0, c.width, c.height);
 
-	ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-	ctx.font = font_size + "px";
-
-    var query = queries[Math.floor(Math.random()*queries.length)];
+	ctx.fillStyle = '#fff';
+	ctx.font = font_size + "px Arial";
+    var queryIndex = Math.floor(Math.random()*queries.length);
+    var query = queries[queryIndex];
+    var offset = query.length*font_size;
     currentQuery = query;
-    ctx.fillText(query, Math.floor(Math.random()*c.width), Math.floor(Math.random()*c.height));
+    queryIndex === 42 && getQueries();
+
+    ctx.fillText(query, Math.floor(Math.random()*(c.width + offset)) - offset, Math.floor(Math.random()*c.height));
 }
 
-setInterval(draw, 88);
+setInterval(draw, 42);
 
 
 
